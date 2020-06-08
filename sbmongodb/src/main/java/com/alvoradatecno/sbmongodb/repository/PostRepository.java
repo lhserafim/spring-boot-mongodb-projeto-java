ef3a82f83390f8aefe0a60062a9241b9124c8d9a
Luiz Henrique Sanches Serafim <a29061@somoseducacao.com.br>
2020-06-08 19:29:44 -0300

    355. Consulta simples com @Query

--- sbmongodb/src/main/java/com/alvoradatecno/sbmongodb/repository/PostRepository.java
+++ sbmongodb/src/main/java/com/alvoradatecno/sbmongodb/repository/PostRepository.java
@@ -5,6 +5,7 @@ package com.alvoradatecno.sbmongodb.repository;
 
 import com.alvoradatecno.sbmongodb.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -20,5 +21,16 @@ public interface PostRepository extends MongoRepository<Post, String> { // Strin
     //    Consulta: "Buscar posts contendo um dado string no título"
 
     // Implementar consultas simples com query methods
+    // este nome findByTitleContainingIgnoreCase está em conformidade com a documentação acima
     List<Post> findByTitleContainingIgnoreCase(String text); // o Title é o campo que eu quero buscar
+
+    //    Referências:
+    //    https://docs.spring.io/spring-data/mongodb/docs/current/reference/html/
+    //    https://docs.spring.io/spring-data/data-document/docs/current/reference/html/
+    //    https://docs.mongodb.com/manual/reference/operator/query/regex/
+    //    Consulta: "Buscar posts contendo um dado string no título"
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }") // o ?0 é referente ao primeiro paramentro. aqui no caso o String text
+                                                         // o i é um parametro p/ ignorar maiusculas e minusculas
+    List<Post> searchTitle(String text); // Aqui pode ser qualquer nome
 }
