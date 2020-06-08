b7e96c61fb129001abae1f6705a22eb48f80014e
Luiz Henrique Sanches Serafim <a29061@somoseducacao.com.br>
2020-06-08 08:36:25 -0300

    342. Usando padrão DTO para retornar usuários

--- sbmongodb/src/main/java/com/alvoradatecno/sbmongodb/resources/UserResource.java
+++ sbmongodb/src/main/java/com/alvoradatecno/sbmongodb/resources/UserResource.java
@@ -5,6 +5,7 @@
 package com.alvoradatecno.sbmongodb.resources;
 
 import com.alvoradatecno.sbmongodb.domain.User;
+import com.alvoradatecno.sbmongodb.dto.UserDTO;
 import com.alvoradatecno.sbmongodb.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 // Controladores REST conversam diretamente com a aplicação do cliente
 @RestController // Anotação p/ identificar que é um recurso REST
@@ -25,7 +27,7 @@ public class UserResource {
 
     // Para dizer que este método é um endpoint deste caminho (/users) eu preciso colocar a anotação
     @RequestMapping(method = RequestMethod.GET) // tmb poderia usar o @GetMapping()
-    public ResponseEntity<List<User>> findAll() { // ResponseEntity é um objeto do Spring que encapsula estrutura p/ retornar em HTTP
+    public ResponseEntity<List<UserDTO>> findAll() { // ResponseEntity é um objeto do Spring que encapsula estrutura p/ retornar em HTTP
         // User maria = new User("1", "Maria Brown", "maria@gmail.com");
         // User alex = new User("2", "Alex Green", "alex@gmail.com");
         // List é apenas uma interface. No java interfaces NÃO PODEM SER INSTANCIADAS.
@@ -34,6 +36,12 @@ public class UserResource {
         // list.addAll(Arrays.asList(maria, alex));
 
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        // Como minha lista agora é de DTO preciso fazer a conversão dos elementos da lista original
+        List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        // .stream() - Converter para stream p/ ser compátivel com lambda
+        // .map() para pegar cada objeto x (pode ser o nome que eu quiser. X é User pq vem da list
+        // passo o X como argumento
+        // transformar em lista novamente .collect(Collectors.toList()
+        return ResponseEntity.ok().body(listDto);
     }
 }
